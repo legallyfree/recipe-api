b6d0c42be70fed918b37951a50e626ba968f778a
legallyfree <34919268+legallyfree@users.noreply.github.com>
2021-08-02 16:32:51 -0300

    Revert "recipe data model"
    This reverts commit c34a44c402a3e74b9ecd0a56a72dff0e7558d587.

--- main.go
+++ main.go
@@ -1,18 +1,6 @@
 package main
 
-import (
-	"time"
-
-	"github.com/gin-gonic/gin"
-)
-
-type Recipe struct {
-	Name         string    `json:"name"`
-	Tags         []string  `json:"tags"`
-	Ingredients  []string  `json:"ingredients"`
-	Instructions []string  `json:"instructions"`
-	PublishedAt  time.Time `json:"publishedAt"`
-}
+import "github.com/gin-gonic/gin"
 
 func main() {
 	router := gin.Default()
